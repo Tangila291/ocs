940b8e39f64f21f8f3290a3f7159c335af885b96
Mahim Islam <tangilaislam11.com>
2024-10-31 17:36:34 +0600

    route, method, controller

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,9 @@
 <?php
-
+use App\Http\Controllers\HomePageController;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\AboutUsController;
+use App\Http\Controllers\ContactUsController;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/',[HomePageController::class,'home']);
+Route::get('/about-us',[AboutUsController::class,'about']);
+Route::get('/contact-us',[ContactUsController::class,'contact']);
